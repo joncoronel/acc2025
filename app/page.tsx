330b956be48baae73f552839af292802193e51cc
joncoronel <59675970+joncoronel@users.noreply.github.com>
2025-04-26 20:32:51 -0700

    Update text in Home component for improved clarity

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly here.
+            Save and see your changes instantly here yes.
           </li>
         </ol>
 
